99eb658bb55380b258c977c56b30ae42943e7402
auvito <aureliusfnvito@gmail.com>
2020-10-07 01:24:44 +0800

    OAuth implemented

---
+++ config/config.exs
@@ -0,0 +1,37 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "qtae+T9kLjiipgQBs25ppdZAcfUSK2EomaP+de4Zjqj1TN8DubU5R01nQdwP3jJK",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
+
+# Configure Github Ueberauth OAuth
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [default_scope: "user,user:email,public_repo"]}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "85f3772e54d3e2d457c5",
+  client_secret: "146463e8f81ad10c08cac4b29d556951e85e943b"
